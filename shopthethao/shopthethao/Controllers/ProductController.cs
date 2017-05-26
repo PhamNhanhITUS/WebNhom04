d61e1ddded37871a6c085e50b9902873dde7ac14
phamnhanh <phamnhanh@PHMNHANHEFFE>
2017-05-27 01:09:26 +0700

    Chi tiết sản phẩm, trang cửa hàng

---
+++ shopthethao/shopthethao/Controllers/ProductController.cs
@@ -0,0 +1,49 @@
+﻿using shopthethao.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace shopthethao.Controllers
+{
+    public class ProductController : Controller
+    {
+        //GET: Product
+       shopthethaoEntities db = new shopthethaoEntities();
+        public ActionResult Index()
+        {
+            ViewBag.ShowCategory = ShowCategory();
+            ViewBag.ShowManufacturer = ShowManufacturer();
+            return View();
+        }
+        public ActionResult Detail(int? id)
+        {
+            if (id.HasValue == false)
+            {
+                return RedirectToAction("Index", "Product");
+            }
+
+            var p = db.SanPhams.First(x => x.MaSanPham == id);
+            return View(p);
+        }
+        public List<LoaiSanPham> ShowCategory()
+        {
+            return db.LoaiSanPhams.ToList();
+        }
+        public ActionResult Category(int? id)
+        {
+            var list = db.LoaiSanPhams.Where(u=>u.MaLoaiSanPham == id).ToList();
+            return View(list);
+        }
+        public List<HangSanXuat> ShowManufacturer()
+        {
+            return db.HangSanXuats.ToList();
+        }
+        public ActionResult Manufacturer(int? id)
+        {
+            var list =  db.HangSanXuats.Where(u=>u.MaHangSanXuat == id).ToList();
+            return View(list);
+        }
+    }
+}
