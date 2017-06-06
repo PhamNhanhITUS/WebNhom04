6cfe440c0b38f8dc12cfcd0b8f0ef8d34bab1e8d
TranThinh96 <tranvanthinh81096@gmail.com>
2017-06-07 05:53:18 +0700

    Thêm form sửa hãng sản xuất, loại sp, tài khoản...

--- shopthethao/shopthethao/Areas/Admin/Controllers/UserController.cs
+++ shopthethao/shopthethao/Areas/Admin/Controllers/UserController.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using shopthethao.Models;
 
 namespace shopthethao.Areas.Admin.Controllers
 {
     public class UserController : Controller
     {
+        shopthethaoEntities2 db = new shopthethaoEntities2();
         // GET: Admin/User
         public ActionResult Index()
         {
-            return View();
+            return View(db.TaiKhoans.ToList());
+        }
+
+        public ActionResult Change(int? ID)
+        {
+            ViewBag.User = db.LoaiTaiKhoans.ToList();
+            var list = db.TaiKhoans.First(x => x.MaTaiKhoan == ID);
+            return View(list);
         }
 
         public ActionResult Delete()
