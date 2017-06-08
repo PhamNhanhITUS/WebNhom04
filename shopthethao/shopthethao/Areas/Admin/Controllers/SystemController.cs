82c45cd20b4d99f101651207412308f666350ad4
TranThinh96 <tranvanthinh81096@gmail.com>
2017-06-09 04:46:39 +0700

    Danh sách nhân viên

--- shopthethao/shopthethao/Areas/Admin/Controllers/SystemController.cs
+++ shopthethao/shopthethao/Areas/Admin/Controllers/SystemController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using shopthethao.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,10 @@ namespace shopthethao.Areas.Admin.Controllers
     public class SystemController : Controller
     {
         // GET: Admin/System
+        shopthethaoEntities2 db = new shopthethaoEntities2();
         public ActionResult Index()
         {
-            return View();
+            return View(db.Admins.ToList());
         }
     }
 }
