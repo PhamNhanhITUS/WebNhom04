eed4c62341da87343be8a502edb06fca078f62db
TranThinh96 <tranvanthinh81096@gmail.com>
2017-06-10 01:34:38 +0700

    Xem chi tiết đơn hàng, in hóa đơn...

--- shopthethao/shopthethao/Areas/Admin/Controllers/OderController.cs
+++ shopthethao/shopthethao/Areas/Admin/Controllers/OderController.cs
@@ -15,6 +15,17 @@ namespace shopthethao.Areas.Admin.Controllers
         {
             return View(db.DonDatHangs.Where(x => x.BiXoa == false).ToList());
         }
-
+        public ActionResult Detail(int? ID)
+        {
+            return View(db.DonDatHangs.First(x => x.MaDonDatHang == ID && x.BiXoa == false));
+        }
+        public ActionResult Change()
+        {
+            return View();
+        }
+        public ActionResult Delete()
+        {
+            return View();
+        }
     }
 }
