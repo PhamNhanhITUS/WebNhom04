fc08f79489a5780c58d05f2e1cec3002a34bfa63
TranThinh96 <tranvanthinh81096@gmail.com>
2017-06-11 11:24:26 +0700

    thêm file sql trigger cập nhật số lượng tồn và số lượng mua

---
+++ trigger.sql
@@ -0,0 +1,24 @@
+CREATE TRIGGER capNhatSoLuongTon
+ON DonDatHang
+for update as
+if update(MaTinhTrang)
+begin
+	declare @maDonDatHang int
+	declare @maTinhTrang int
+	select @maDonDatHang=MaDonDatHang, @maTinhTrang=MaTinhTrang from inserted
+	if @maTinhTrang=3
+	begin
+		declare cur cursor for select MaSanPham, SoLuong from ChiTietDonDatHang where MaDonDatHang = @maDonDatHang
+		open cur
+		declare @MaSanPham int
+		declare @SoLuong int
+		fetch next from cur into @MaSanPham, @SoLuong
+		while @@FETCH_STATUS =0
+		begin
+			update SanPham
+			set SoLuongBan=SoLuongBan+@SoLuong, SoLuongTon=SoLuongTon-@SoLuong
+			where MaSanPham=@MaSanPham
+			fetch next from cur into @MaSanPham, @SoLuong
+		end
+	end
+end
